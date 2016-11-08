18ef2681576f11bd6881308d960831e132342dcc
mhendri <mhendrickson91@me.com>
2016-11-07 21:22:07 -0500

    load signup page with partial DB functionality

--- app.py
+++ app.py
@@ -6,35 +6,35 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/users'
 db = SQLAlchemy(app)
 
-# Create our database model
-# class User(db.Model):
-#     __tablename__ = "useraccounts"
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True)
+#Create our database model
+class User(db.Model):
+    __tablename__ = "useraccounts"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
 
-#     def __init__(self, email):
-#         self.email = email
+    def __init__(self, email):
+        self.email = email
 
-#     def __repr__(self):
-#         return '<E-mail %r>' % self.email
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
 
 # Set "homepage" to index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/showSignUp', methods =['POST'])
-# def showSignUp():
-#     email = None
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         # Check that email does not already exist (not a great query, but works)
-#         if not db.session.query(User).filter(User.email == email).count():
-#             reg = User(email)
-#             db.session.add(reg)
-#             db.session.commit()
-#             return render_template('index.html')
-#     return render_template('signup.html')
+@app.route('/showSignUp', methods =['GET', 'POST'])
+def showSignUp():
+    email = None
+    if request.method == 'POST':
+        email = request.form['email']
+        # Check that email does not already exist (not a great query, but works)
+        if not db.session.query(User).filter(User.email == email).count():
+            reg = User(email)
+            db.session.add(reg)
+            db.session.commit()
+            return render_template('index.html')
+    return render_template('signup.html')
 
 
 if (__name__)=='__main__':
