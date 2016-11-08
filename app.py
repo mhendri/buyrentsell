d93284d392dcb28a390f57fafca5bb3e80a1c49e
mhendri <mhendrickson91@me.com>
2016-11-07 21:14:50 -0500

    postgres integration

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/users'
+db = SQLAlchemy(app)
 
+# Create our database model
+# class User(db.Model):
+#     __tablename__ = "useraccounts"
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True)
+
+#     def __init__(self, email):
+#         self.email = email
+
+#     def __repr__(self):
+#         return '<E-mail %r>' % self.email
+
+# Set "homepage" to index.html
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template('index.html')
+
+# @app.route('/showSignUp', methods =['POST'])
+# def showSignUp():
+#     email = None
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         # Check that email does not already exist (not a great query, but works)
+#         if not db.session.query(User).filter(User.email == email).count():
+#             reg = User(email)
+#             db.session.add(reg)
+#             db.session.commit()
+#             return render_template('index.html')
+#     return render_template('signup.html')
+
 
 if (__name__)=='__main__':
 	app.run(host='local host', port=5555, debug=false)
