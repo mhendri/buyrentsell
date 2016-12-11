b6650dfaee02bfd0ce28de9ddaef6af96f3682e7
Akbar Mirza <akbarbmirza@gmail.com>
2016-12-11 14:49:07 -0500

    Convert Tabs to Spaces

--- app.py
+++ app.py
@@ -224,17 +224,17 @@ class Post(db.Model):
 ## Flag Model
 ##------------------------------------------------------------------------------
 class Flag(db.Model):
-	__tablename__ = "Flag"
-	flagid = db.Column(db.Integer, primary_key=True)
-	userid = db.Column('userid', db.Integer, db.ForeignKey("Users.id"), unique = False)
-	reason = db.Column('flag_reason', db.String(120), unique=False)
+    __tablename__ = "Flag"
+    flagid = db.Column(db.Integer, primary_key=True)
+    userid = db.Column('userid', db.Integer, db.ForeignKey("Users.id"), unique = False)
+    reason = db.Column('flag_reason', db.String(120), unique=False)
 
-############################################################################
-## CONSTRUCTOR
-############################################################################
-	def __init__(self,userid=None, reason=""):
-		self.userid = userid
-		self.reason = reason
+    ############################################################################
+    ## CONSTRUCTOR
+    ############################################################################
+    def __init__(self,userid=None, reason=""):
+        self.userid = userid
+        self.reason = reason
 
 
 # Need to add few more things:
@@ -263,11 +263,11 @@ admin.add_view(ModelView(Flag, db.session))
 @app.route('/')
 @app.route('/index')
 def index():
-	if not session.get('logged_in'):
-		return render_template('index.html')
-	else:
-		#if logged_in, we should display show_entries
-		return render_template('index.html')
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        #if logged_in, we should display show_entries
+        return render_template('index.html')
 
 # Logging In
 @app.route('/login', methods=['GET', 'POST'])
@@ -299,90 +299,90 @@ def login():
 # Logging Out
 @app.route('/logout')
 def logout():
-	session['logged_in'] = False
-	flash('SUCCESS: Logged Out!')
-	return index()
+    session['logged_in'] = False
+    flash('SUCCESS: Logged Out!')
+    return index()
 
 # # Signing Up
 # @app.route('/signup', methods =['GET', 'POST'])
 # def signup():
-# 	global resultnum
-# 	firstnum = int(random.random() * 10)
-# 	secondnum = int(random.random() * 10)
-# 	resultnum = firstnum + secondnum
-# 	form = SignupForm(request.form)
-# 	if request.method == 'POST' and int(request.form['captcha']) == int(resultnum):
-# 		if form.validate():
-# 			email_exist = User.query.filter_by(email=form.email.data).first()
-# 			if email_exist:
-# 				form.email.errors.append('Email already in use')
-# 				return render_template('signup.html', form=form, page_title = "Sign Up")
-# 			else:
-# 				firstname = form.firstname.data
-# 				lastname = form.lastname.data
-# 				email = form.email.data
-# 				password = form.password.data
-# 				phone = form.phone.data
-# 				entry = User(email, password, firstname, lastname, phone)
-# 				db.session.add(entry)
-# 				db.session.commit()
-# 				return render_template('success.html')
-# 		else:
-# 			firstnum = int(random.random() * 10)
-# 			secondnum = int(random.random() * 10)
-# 			resultnum = firstnum + secondnum
-# 			return render_template('signup.html', form=form)
-# 	return render_template('signup.html', form=SignupForm(), firstnum=firstnum, secondnum=secondnum)
+#     global resultnum
+#     firstnum = int(random.random() * 10)
+#     secondnum = int(random.random() * 10)
+#     resultnum = firstnum + secondnum
+#     form = SignupForm(request.form)
+#     if request.method == 'POST' and int(request.form['captcha']) == int(resultnum):
+#         if form.validate():
+#             email_exist = User.query.filter_by(email=form.email.data).first()
+#             if email_exist:
+#                 form.email.errors.append('Email already in use')
+#                 return render_template('signup.html', form=form, page_title = "Sign Up")
+#             else:
+#                 firstname = form.firstname.data
+#                 lastname = form.lastname.data
+#                 email = form.email.data
+#                 password = form.password.data
+#                 phone = form.phone.data
+#                 entry = User(email, password, firstname, lastname, phone)
+#                 db.session.add(entry)
+#                 db.session.commit()
+#                 return render_template('success.html')
+#         else:
+#             firstnum = int(random.random() * 10)
+#             secondnum = int(random.random() * 10)
+#             resultnum = firstnum + secondnum
+#             return render_template('signup.html', form=form)
+#     return render_template('signup.html', form=SignupForm(), firstnum=firstnum, secondnum=secondnum)
 
 
 # Signing Up
 @app.route('/signup', methods =['GET', 'POST'])
 def signup():
-	if request.method == 'POST':
-		form = SignupForm(request.form)
-		if form.validate():
-			email_exist = User.query.filter_by(email=form.email.data).first()
-			if email_exist:
-				form.email.errors.append('Email already in use')
-				return render_template('signup.html', form=form, page_title = "Sign Up")
-			else:
-				firstname = form.firstname.data
-				lastname = form.lastname.data
-				email = form.email.data
-				password = form.password.data
-				phone = form.phone.data
-				entry = User(email, password, firstname, lastname, phone)
-				db.session.add(entry)
-				db.session.commit()
-				flash('You have sucessfully signed up and will have access shortly! Thank you for your patience')
-				return render_template('index.html')
-		else:
-			return render_template('signup.html', form=form)
-	return render_template('signup.html', form=SignupForm())
+    if request.method == 'POST':
+        form = SignupForm(request.form)
+        if form.validate():
+            email_exist = User.query.filter_by(email=form.email.data).first()
+            if email_exist:
+                form.email.errors.append('Email already in use')
+                return render_template('signup.html', form=form, page_title = "Sign Up")
+            else:
+                firstname = form.firstname.data
+                lastname = form.lastname.data
+                email = form.email.data
+                password = form.password.data
+                phone = form.phone.data
+                entry = User(email, password, firstname, lastname, phone)
+                db.session.add(entry)
+                db.session.commit()
+                flash('You have sucessfully signed up and will have access shortly! Thank you for your patience')
+                return render_template('index.html')
+        else:
+            return render_template('signup.html', form=form)
+    return render_template('signup.html', form=SignupForm())
 
 @app.route('/post', methods = ['GET', 'POST'])
 def post():
-	if request.method == 'POST':
-		form = PostForm(request.form)
-		if form.validate():
-			title = form.title.data
-			price = form.price.data
-			descr = form.descr.data
-			image = form.image.data
-			date = datetime.utcnow()
-			category = form.category.data
-			entry = Post(1,title, price, descr, date, category, image)
-			db.session.add(entry)
-			db.session.commit()
-			flash('Item Posted!')
-			return render_template('index.html')
-		else:
-			return render_template('post.html', form=form)
+    if request.method == 'POST':
+        form = PostForm(request.form)
+        if form.validate():
+            title = form.title.data
+            price = form.price.data
+            descr = form.descr.data
+            image = form.image.data
+            date = datetime.utcnow()
+            category = form.category.data
+            entry = Post(1,title, price, descr, date, category, image)
+            db.session.add(entry)
+            db.session.commit()
+            flash('Item Posted!')
+            return render_template('index.html')
+        else:
+            return render_template('post.html', form=form)
 
-	return render_template('post.html', form=PostForm())
+    return render_template('post.html', form=PostForm())
 
 if (__name__)=='__main__':
-	app.run(host='localhost', port=5000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
 
 # User profile pages accessible by /user/id
 @app.route('/user/<id>', methods=['GET', 'POST'])
