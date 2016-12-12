ef30ffdb645b00c510871aac077cd3ef53eb7bec
edzh <edwinzzhou@gmail.com>
2016-12-11 19:36:30 -0500

    Add validation for post and signup forms

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ from wtforms import validators, ValidationError
 class SignupForm(Form):
 	firstname = TextField("First Name", [validators.Required("Please enter your first name.")])
 	lastname = TextField("Last Name", [validators.Required("Please enter your last name.")])
-	email = TextField("Email",[validators.Required("Please enter your email address."),
+	email = TextField("Email", [validators.Required("Please enter your email address."),
 		validators.Email("Please enter your email address")])
 	password = PasswordField("Passsword", [validators.Required("Please enter a password.")])
 	phone = IntegerField("Phone", [validators.Required("Please enter a phone number.")])
